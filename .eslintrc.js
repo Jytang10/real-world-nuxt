aecb37852e924385323a8b77617ad3d97c2dc87a
James Tang <2163103@FITNESSINTL.COM>
2020-02-06 13:16:53 -0800

    Completed app

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,17 +7,13 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ["es-beautifier"],
+  extends: ["plugin:vue/essential", "plugin:es-beautifier/standard", "@vue/typescript"],
   // add your custom rules here
   rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "comma-dangle": ["error", "never"],
+    "linebreak-style": "off"
   }
 }
